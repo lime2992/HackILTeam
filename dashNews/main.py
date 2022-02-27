effd33039cf195bcef7b2b9d788482d6751882af
Hayden K <haydenk643@gmail.com>
2022-02-27 08:14:48 -0600

    Added new branch for styling (2.0)

--- dashNews/main.py
+++ dashNews/main.py
@@ -2,17 +2,46 @@ import outline
 import dash_bootstrap_components as dbc
 from dash import dcc, Input, Output, html, State
 
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Col(html.Div('NewsFlow', style={'color': 'white', 'fontSize': 45, 'bold': True,
+                                                }), style={'marginRight': 40, 'marginLeft': 10}),
+            ''''
+            dbc.Row([
+                dbc.Col(dbc.Input(id="searchField", placeholder="News topics", size="md", className="mb-3",
+                                  style={'width': 500, 'height': 40, 'marginTop': 20,
+                                         'marginRight': 20}), width="auto"),
+                dbc.Col(dcc.Dropdown(outline.items, id="country", placeholder="CountryDrop", style={'marginRight': 20}),
+                    width=3),
+                dbc.Col(dbc.Button("Go!", id="searchButton", className="me-2", n_clicks=0, style={'width': 100,
+                                                                                                  'height': 40,
+                                                                                                  'marginRight': 20}),
+                        width="auto"),
+            ], justify="end"),
+            '''
+
+
+        ]
+    ),
+    color="dark",
+    dark=True,
+)
+
 outline.app.layout =  html.Div(
         [
-            dbc.Row(
-                [
-                    dbc.Col(dbc.Input(id="searchField", placeholder="News topics", size="md", className="mb-3"),
-                            width="auto"),
-                    dbc.Col(dbc.Button("Go!", id="searchButton", className="me-2", n_clicks=0), width="auto"),
-                    dbc.Col(dcc.Dropdown(outline.items, id="country", placeholder="CountryDrop"), width=3),
-                ]
-                , justify="end"),
-            dbc.Row([html.H1("Top News Articles")]),
+            navbar,
+
+            #dbc.Row(
+            #    [
+             #       dbc.Col(dbc.Input(id="searchField", placeholder="News topics", size="md", className="mb-3"),
+              #              width="auto"),
+               #     dbc.Col(dbc.Button("Go!", id="searchButton", className="me-2", n_clicks=0), width="auto"),
+                #    dbc.Col(dcc.Dropdown(outline.items, id="country", placeholder="CountryDrop"), width=3),
+                #]
+                #, justify="end"),
+            dbc.Row([html.H1("Top News Articles")], style={'marginLeft': 10, 'marginBottom': 10, 'marginTop': 25}),
             dbc.Row(
                 [
                     outline.newsCards,
