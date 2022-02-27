d54974b6a9c1f9ce46fe871bc7c2cd2c228f1d19
elainemwang <elaine.m.wang@gmail.com>
2022-02-27 07:30:49 -0600

    remove unnecessary calls

--- dashNews/outline.py
+++ dashNews/outline.py
@@ -23,19 +23,31 @@ items.pop()
 
 api = NewsApiClient(api_key='77e164585ab1422893fc26ae68be9f05')
 
+default_top = api.get_top_headlines()
 top = api.get_top_headlines()
+
+default_tweets = get_trending_tweets(1)
 tweets = get_trending_tweets(1)
+
+tweets_dict = {}
+news_dict = {}
+
 
 def setVars(location, query):
     global tweets
     global top
-    top = api.get_top_headlines()
-    tweets = get_trending_tweets(1)
+    top = default_top
+    tweets = default_tweets
     if location is None:
-        tweets = get_trending_tweets(1)
+        tweets = default_tweets
     else:
         print(countries)
-        tweets = get_trending_tweets(countries[location]["twitterid"])
+        if location in tweets_dict:
+            print("used dict")
+            tweets = tweets_dict[location]
+        else:
+            tweets = get_trending_tweets(countries[location]["twitterid"])
+            tweets_dict[location] = tweets
         print(tweets)
 
     if query is not None and location is not None:
@@ -44,12 +56,17 @@ def setVars(location, query):
 
     if query is None and location is not None:
         print(countries[location]["newsid"])
-        top = api.get_top_headlines(country=countries[location]["newsid"])
+        if location in news_dict:
+            top = news_dict[location]
+        else:
+            top = api.get_top_headlines(country=countries[location]["newsid"])
+            news_dict[location] = top
 
     if query is not None and location is None:
         top = api.get_top_headlines(q=query)
 
     print(top)
+
 
 def generateCard(i):
     card = dbc.Card(
@@ -88,6 +105,7 @@ def generateCard(i):
 
     return card
 
+
 news_articles = []
 for index, article in enumerate(top['articles']):
     news_articles.append(dbc.AccordionItem(
@@ -106,13 +124,13 @@ newsCards = dbc.Accordion(
 
 hold_tweet_list = []
 hold_tweet_list.append(dbc.ListGroupItem([
-            html.Div(
-                [
-                    html.H5("Trending Tweets", className="mb-1"),
-                ],
-                className="d-flex w-100 justify-content-between",
-            ),
-        ]),)
+    html.Div(
+        [
+            html.H5("Trending Tweets", className="mb-1"),
+        ],
+        className="d-flex w-100 justify-content-between",
+    ),
+]), )
 
 for i in range(10):
     hold_tweet_list.append(dbc.ListGroupItem(tweets[i]['name'], href=tweets[0]['url']))
